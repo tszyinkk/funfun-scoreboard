6a8e0efa9f5a1e28e543525824de6a8f6a625495
tszyinkk <121917735+tszyinkk@users.noreply.github.com>
2026-09-11 00:30:23 -0400

    Set dealer multipliers to one by default

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "funfun-scoreboard-v19";
+const CACHE_NAME = "funfun-scoreboard-v20";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=19",
-  "./app.js?v=19",
+  "./style.css?v=20",
+  "./app.js?v=20",
   "./manifest.webmanifest",
   "./icons/icon.svg",
   "./icons/icon-192.png",
