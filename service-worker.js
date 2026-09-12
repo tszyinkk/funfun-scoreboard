e3cf2ce1a13c20f3ee7bb0cd20844d8de273f782
tszyinkk <121917735+tszyinkk@users.noreply.github.com>
2026-09-11 23:41:48 -0400

    Add light and dark score panels

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "funfun-scoreboard-v30";
+const CACHE_NAME = "funfun-scoreboard-v32";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=30",
-  "./app.js?v=30",
+  "./style.css?v=32",
+  "./app.js?v=32",
   "./manifest.webmanifest",
   "./icons/icon.svg",
   "./icons/icon-192.png",
